bad0207f7dc6d0f9b5bce23b2ceafc3759065279
Djimi02 <mitko.manolev1@abv.bg>
2023-05-10 13:28:02 +0200

    transmission model and repository added

---
+++ CarCatalog/src/main/java/com/example/CarCatalog/models/Transmission.java
@@ -0,0 +1,34 @@
+package com.example.CarCatalog.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Transmission {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    public Transmission() {
+    }
+
+    public Transmission(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
