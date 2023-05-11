b0a4a16e4a02a954d287ae15e4810394d5ee1425
Djimi02 <mitko.manolev1@abv.bg>
2023-05-11 23:34:58 +0200

    removed unused imports

--- CarCatalog/src/main/java/com/example/CarCatalog/models/Car.java
+++ CarCatalog/src/main/java/com/example/CarCatalog/models/Car.java
@@ -1,7 +1,5 @@
 package com.example.CarCatalog.models;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
